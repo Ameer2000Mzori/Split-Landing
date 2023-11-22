e1acc5da37db97bfd69e809b32f93def4c0c6ffb
Ameer <ameerapex496@gmail.com>
2023-11-22 18:47:30 +0100

    added function to playstation button

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// selecting our elements
+var psBuyBtn = document.getElementsByClassName("ps-buy-Btn")[0];
+var xboxBuyBtn = document.getElementsByClassName("xbox-buy-Btn")[0];
+var headerPage = document.getElementsByClassName("header")[0];
+// our functions
+var psHandler = function () {
+    headerPage.innerHTML = "";
+    headerPage.innerHTML = "\n  <section class=\"plays-Wrapper\">\n      <div class=\"plays-Text-Wrapper\">\n        <h1>PLAYSTATION</h1>\n        <ol>\n          <li>BEST CONSOLE</li>\n          <li>BEST SELLER</li>\n          <li>BEST DLC</li>\n        </ol>\n        <button>\n          <a href=\"https://www.playstation.com/nl-nl/en-en/\" target=\"_blank\"\n            >BUY NOW</a\n          >\n        </button>\n        <button>\n          <a href=\"./index.html\">GO BACK</a>\n        </button>\n      </div>\n    </section>\n  ";
+};
+var xbHandler = function () { };
+// our eventlinsters
+psBuyBtn.addEventListener("click", psHandler);
+xboxBuyBtn.addEventListener("click", xbHandler);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,40 @@
-console.log("hallo worl");
+// selecting our elements
+const psBuyBtn = document.getElementsByClassName(
+  "ps-buy-Btn"
+)[0] as HTMLElement;
+const xboxBuyBtn = document.getElementsByClassName(
+  "xbox-buy-Btn"
+)[0] as HTMLElement;
+const headerPage = document.getElementsByClassName("header")[0] as HTMLElement;
+
+// our functions
+
+const psHandler = (): void => {
+  headerPage.innerHTML = "";
+  headerPage.innerHTML = `
+  <section class="plays-Wrapper">
+      <div class="plays-Text-Wrapper">
+        <h1>PLAYSTATION</h1>
+        <ol>
+          <li>BEST CONSOLE</li>
+          <li>BEST SELLER</li>
+          <li>BEST DLC</li>
+        </ol>
+        <button>
+          <a href="https://www.playstation.com/nl-nl/en-en/" target="_blank"
+            >BUY NOW</a
+          >
+        </button>
+        <button>
+          <a href="./index.html">GO BACK</a>
+        </button>
+      </div>
+    </section>
+  `;
+};
+
+const xbHandler = (): void => {};
+
+// our eventlinsters
+psBuyBtn.addEventListener("click", psHandler);
+xboxBuyBtn.addEventListener("click", xbHandler);
